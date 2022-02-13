bbd153be555912ce9c02430f69ee138e9f3ec501
Ricky <drickyriverab@gmail.com>
2022-02-13 20:34:36 +0100

    hi imo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../Task';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+
+  private apiUrl = 'http://localhost:3000/tasks';
+
+  constructor(private http: HttpClient) {}
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+}
